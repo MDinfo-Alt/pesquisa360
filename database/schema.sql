aaccbe8653bdf7a44e146b4ae2855ce85b0d9415
Marlon Dias <81657991+MDinfo-Alt@users.noreply.github.com>
2026-08-08 12:35:45 -0300

    feat: create segmentos table

--- database/schema.sql
+++ database/schema.sql
@@ -1,2 +1,9 @@
+
 -- Pesquisa360
 -- Schema inicial do banco de dados
+
+CREATE TABLE segmentos (
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    ativo BOOLEAN NOT NULL DEFAULT TRUE
+);

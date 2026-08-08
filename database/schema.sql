700f4e0de709fc547662d164f74d8186743b0472
Marlon Dias <81657991+MDinfo-Alt@users.noreply.github.com>
2026-08-08 15:36:16 -0300

    feat: create entrevistas table

--- database/schema.sql
+++ database/schema.sql
@@ -24,3 +24,30 @@ CREATE TABLE empresas (
         FOREIGN KEY (segmento_id)
         REFERENCES segmentos(id)
 );
+CREATE TABLE contatos (
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    empresa_id INTEGER NOT NULL,
+    nome VARCHAR(150) NOT NULL,
+    cargo VARCHAR(100),
+
+    CONSTRAINT fk_contatos_empresa
+        FOREIGN KEY (empresa_id)
+        REFERENCES empresas(id)
+);
+CREATE TABLE entrevistas (
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    empresa_id INTEGER NOT NULL,
+    contato_id INTEGER,
+    data_entrevista DATE NOT NULL,
+    observacoes TEXT,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_entrevistas_empresa
+        FOREIGN KEY (empresa_id)
+        REFERENCES empresas(id),
+
+    CONSTRAINT fk_entrevistas_contato
+        FOREIGN KEY (contato_id)
+        REFERENCES contatos(id)
+);
